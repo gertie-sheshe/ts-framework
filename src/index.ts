225aa80bfa31e9589095546b9093fae2e2550019
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2020-09-11 15:50:09 +0300

    set and get for User class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 console.log('wauuu');
+
+import { User } from './models/User';
+
+const user = new User({ name: 'GeeGee', age: 78 });
+
+user.set({ age: 2 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
