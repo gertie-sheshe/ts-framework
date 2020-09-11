cb78d8b59987d12221725c196304da5224410e66
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2020-09-11 16:55:05 +0300

    eventing and server

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,12 @@ const user = new User({ name: 'GeeGee', age: 78 });
 
 user.set({ age: 2 });
 
+user.on('click', () => console.log('Click!!!'));
+user.on('click', () => console.log('Click!!! Twicce'));
+user.on('change', () => console.log('We change'));
+user.on('hover', () => console.log('Hover me up'));
+
+user.trigger('click');
+
 console.log(user.get('name'));
 console.log(user.get('age'));
