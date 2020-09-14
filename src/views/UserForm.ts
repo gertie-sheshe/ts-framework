6882c0d96fa85c073131d5971e259a77d4b44a9e
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2020-09-14 16:33:55 +0300

    bind events

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -15,12 +15,27 @@ export class UserForm {
     return `<div>
       <h1>User Form</h1>
       <input/>
+      <button>Click Me</button>
     </div>`;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector]: string[] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
   }
 
   render(): void {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
 
     this.parent.append(templateElement.content);
   }
