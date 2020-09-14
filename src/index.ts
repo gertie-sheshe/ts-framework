110f5a8078a0eb4984f3958052fd1792084cb490
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2020-09-14 16:17:48 +0300

    setup form view and render

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,6 @@
-import { User, UserProps } from './models/User';
-import { Collection } from './models/Collection';
+import { User } from './models/User';
+import { UserForm } from './views/UserForm';
 
-const collection = User.buildUserCollection();
+const userForm = new UserForm(document.getElementById('root'));
 
-collection.fetch();
-
-collection.on('change', () => {
-  console.log('WAUUUUU', collection);
-});
-
-// const user = User.buildUser({ name: 'GeeGee', age: 78 });
-
-// user.save();
-
-// user.on('save', () => {
-//   console.log('Saaaaaave', user.get('id'));
-// });
-
-// // user.save();
-
-// user.save().then(() => {
-//   // user.fetch();
-//   console.log('Saaaaaave', user.get('id'));
-// });
-
-// user.set({ age: 2 });
-
-// user.on('click', () => console.log('Click!!!'));
-// user.on('click', () => console.log('Click!!! Twicce'));
-// user.on('change', () => console.log('We change'));
-// user.on('hover', () => console.log('Hover me up'));
-
-// user.trigger('click');
-
-// console.log(user.get('name'));
-// console.log(user.get('age'));
+userForm.render();
---
+++ src/views/UserForm.ts
@@ -0,0 +1,27 @@
+export class UserForm {
+  constructor(public parent: Element) {}
+
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick,
+    };
+  }
+
+  onButtonClick(): void {
+    console.log('Clicked');
+  }
+
+  template(): string {
+    return `<div>
+      <h1>User Form</h1>
+      <input/>
+    </div>`;
+  }
+
+  render(): void {
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = this.template();
+
+    this.parent.append(templateElement.content);
+  }
+}
